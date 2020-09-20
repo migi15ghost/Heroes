b6e45cfae610239fc0a362f854ee320f450de43d
Miguel Poma <miguel.poma.dev@gmail.com>
2020-09-20 16:49:09 +0100

    [ADD] Search screen

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -5,6 +5,7 @@ import { Navbar } from '../components/ui/NavBar'
 import { DCScreen } from '../components/dc/DCScreen'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
 import { HeroScreen } from '../components/heroes/HeroScreen'
+import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashboardRoutes = () => {
     return (
@@ -16,6 +17,7 @@ export const DashboardRoutes = () => {
                   <Route exact path="/marvel" component={MarvelScreen} />
                   <Route exact path="/hero/:heroeId" component={HeroScreen} />
                   <Route exact path="/dc" component={DCScreen} />
+                  <Route exact path="/search" component={SearchScreen} />
                   <Redirect to="/marvel"  />
               </Switch>                 
             </div>

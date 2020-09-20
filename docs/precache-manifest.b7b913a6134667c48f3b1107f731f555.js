9d9f2d4b6a773888dc40014fd1a083ff277bb082
Miguel Poma Dev <miguel.poma.dev@gmail.com>
2020-09-20 17:42:34 +0100

    [ADD] Build proyect

---
+++ docs/precache-manifest.b7b913a6134667c48f3b1107f731f555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0467dd59955040c5cbd18b3c2e948ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00411947576e35d4dd36",
+    "url": "/static/js/2.69bb7c0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69bb7c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e693f87e5fb5e6bd8306",
+    "url": "/static/js/main.d8a630b4.chunk.js"
+  },
+  {
+    "revision": "bf6f231da1ac296d9531",
+    "url": "/static/js/runtime-main.42386516.js"
+  }
+]);
